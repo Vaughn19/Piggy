2d7ec72d8815c5d9bbb923613c26ff9e4aba6367
Vaughn19 <70531427+Vaughn19@users.noreply.github.com>
2020-10-15 11:08:38 -0400

    error in code

--- student.py
+++ student.py
@@ -275,7 +275,7 @@ class Piggy(PiggyParent):
         print("-------- [ Press CTRL + C to stop me ] --------\n")
         print("-----------! NAVIGATION ACTIVATED !------------\n")
         
-        exit_angle = self.get_heading()
+        exit_ang = self.get_heading()
         # because ive written down the exits angle, at anytime i can use:
         # selfturn_to_deg(exit_ang)
         turn_count = 0 
